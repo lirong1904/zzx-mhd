f010d5d2cf58e996887c1f28271a4048b3e7e5c2
李荣 <1599853964@qq.com>
2020-05-04 22:21:08 +0800

    bugfix: autoprefixer 的配置处理

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '../views/Home'
+import Home from '@/views/Home'
 import Classify from '@/views/classify'
 import Hello from '@/views/Hello'
 import Favorite from '@/views/Hello/Favorite'
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,56 @@
 <template>
   <div class="page-home">
-    <h1>首页</h1>
+    <header class="index-header">
+      <a href="mine.html?cpid=0">
+        <div class="header-user">
+          <div class="user-btn"></div>
+        </div>
+      </a>
+      <div class="header-logo"></div>
+      <a href="search.html?cpid=0">
+        <div class="header-search"></div>
+      </a>
+    </header>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home"
+};
 </script>
+<style lang="scss" scoped>
+.page-home {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+.index-header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  height: 44px;
+  box-sizing: border-box;
+  .user-btn {
+    
+    width: 25px;
+    height: 25px;
+    background: url('~@/assets/icon/user-btn.png')no-repeat;
+    background-size:100% ;
+  }
+  .header-logo {
+    width: 92px;
+    height: 28px;
+    background:url('~@/assets/icon/login.png')no-repeat;
+    background-size:100% ;
+  }
+  .header-search {
+    width: 25px;
+    height: 25px;
+    background: url('~@/assets/icon/search.png')no-repeat;
+     background-size:100% ;
+  }
+}
+}
+</style>

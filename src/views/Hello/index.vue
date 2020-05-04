2f76d3b3c7944dd8c034a060b42fddd2465f011f
李荣 <1599853964@qq.com>
2020-05-04 17:16:31 +0800

    路由基础搭建

---
+++ src/views/Hello/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-hello">
+    
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
---
+++ src/views/Vip/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="page-vip">
+    <h1>Vip专区</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Vip"
+};
+</script>
